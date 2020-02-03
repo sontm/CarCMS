294cedc984181a4f7e693d30de151265d4f5503a
SanSanJP <sontm.uet.vnu@gmail.com>
2020-02-04 00:12:28 +0700

    [ncov] data 02/03 15:00 GMT

--- backend/controllers/nCoVCtrl.js
+++ backend/controllers/nCoVCtrl.js
@@ -13,12 +13,10 @@ var mailer = require("nodemailer");
 var xoauth2 = require('xoauth2');
 const ExtractJWT = passportJWT.ExtractJwt;
 
-var testVar = 1;
-
 const NCOV_DATA = {
     vietnam_province: [
     {
-        date:"2020-02-03T02:00:00.007Z",
+        date:"2020-02-03T15:00:00.007Z",
         provinces:[
             {
                 name:"TP.HCM",
@@ -286,11 +284,12 @@ china_province: [
   ],
   data: [
       {
-        date:"2020-02-03T02:00:00.007Z",
+        date:"2020-02-03T15:00:00.007Z",
         world: {
-            case: 17388,
+            case: 17488,
             death: 362,
-            risk:"High"
+            curve: 523,
+            risk:"Very High"
         },
         tranmission_rate_min: 3.0,
         tranmission_rate_max: 4.0,
@@ -300,12 +299,12 @@ china_province: [
         countries: [
         {
             name:"Mainland China",
-            case: 17206,
+            case: 17302,
             death: 361,
             suspect: null,
             isolate: null,
             severe: 2296,
-            curve: 475,
+            curve: 512,
             risk:"Very High"
         },{
             name:"Japan",
@@ -326,13 +325,24 @@ china_province: [
             death: 0,
         },{
             name:"Hong Kong",
-            case: 14,
+            case: 15,
             death: 0,
         },{
             name:"Australia",
             case: 12,
             death: 0,
             curve: 2
+        },{
+            name:"United States",
+            case: 11,
+            death: 0,
+            note:"+ 1 Case in Snohomish, Washington. Male, Age 30s from Jan.21.\n\n"
+                +"+ 2 Cases in Chicago, Illinois. 1 Female at Age 60s from Jan.24; 1 Male husband from Jan.30\n\n"
+                +"+ 3 Cases in California. 2 from Jan.26 in Orange C., L.A.; 1 Male adult from Jan.31 in Santa Clara C.\n\n"
+                +"+ 1 Case in Maricopa County, Arizona. 1 Student from Jan.26.\n\n"
+                +"+ 1 Case in Boston, Massachusetts. 1 Male at Age 20s From Feb.1.\n\n"
+                +"+ 1 Case in Santa Clara C., California. 1 Female From Feb.2.\n\n"
+                +"+ 2 Case in San Benito C., California. 1 Male and 1 Female at age 50s From Feb.2.\n\n"
         },{
             name:"Taiwan",
             case: 10,
@@ -341,16 +351,6 @@ china_province: [
             name:"Germany",
             case: 10,
             death: 0,
-        },{
-            name:"United States",
-            case: 9,
-            death: 0,
-            note:"+ 1 Case in Snohomish, Washington. Male, Age 30s from Jan.21.\n\n"
-                +"+ 2 Cases in Chicago, Illinois. 1 Female at Age 60s from Jan.24; 1 Male husband from Jan.30\n\n"
-                +"+ 3 Cases in California. 2 from Jan.26 in Orange C., L.A.; 1 Male adult from Jan.31 in Santa Clara C.\n\n"
-                +"+ 1 Case in Maricopa County, Arizona. 1 Student from Jan.26.\n\n"
-                +"+ 1 Case in Boston, Massachusetts. 1 Male at Age 20s From Feb.1.\n\n"
-                +"+ 1 Case in Santa Clara C., California. 1 Female From Feb.2."
         },{
             name:"Malaysia",
             case: 8,
@@ -363,14 +363,18 @@ china_province: [
             name:"Vietnam",
             case: 8,
             death: 0,
-            suspect: 92,
-            isolate: 27,
-            curve: 1,
-            note:"+ 2 bệnh nhân người Trung Quốc.\n\n+ 3 người Việt Nam, trong đó 2 trường hợp đang được cách ly điều trị tại BC Nhiệt đới cơ sở 2, 1 trường hợp tại BVDK tỉnh Thanh Hoá. Cả 3 đều trở về từ Vũ Hán.\n\n+ 1 người Việt Nam là lễ tân tại khách sạn Khánh Hoà, tiếp xúc với 2 người Trung Quốc đang điều trị tại Việt Nam.\n\n+ 1 bệnh nhân có quốc tịch Mỹ, bay về Việt Nam ngày 14/1, quá cảnh tại sân bay Vũ Hán.\n\n+ Sáng 3/2, Bộ Y tế xác nhận nữ bệnh nhân 29 tuổi, là công nhân trở về từ Vũ Hán, dương tính với nCoV."
+            suspect: 304,
+            isolate: 270,
+            curve: 2,
+            note:"+ 2 bệnh nhân người Trung Quốc.\n\n"
+                +"+ 3 người Việt Nam, trong đó 2 trường hợp đang được cách ly điều trị tại BC Nhiệt đới cơ sở 2, 1 trường hợp tại BVDK tỉnh Thanh Hoá. Cả 3 đều trở về từ Vũ Hán.\n\n"
+                +"+ 1 người Việt Nam là lễ tân tại khách sạn Khánh Hoà, tiếp xúc với 2 người Trung Quốc đang điều trị tại Việt Nam.\n\n"
+                +"+ 1 bệnh nhân có quốc tịch Mỹ, bay về Việt Nam ngày 14/1, quá cảnh tại sân bay Vũ Hán.\n\n"
+                +"+ Sáng 3/2, Bộ Y tế xác nhận nữ bệnh nhân 29 tuổi, là công nhân trở về từ Vũ Hán, dương tính với nCoV."
         },{
             name:"France",
             case: 6,
-            death: 0,
+            death: 0
         },{
             name:"UAE",
             case: 5,
@@ -378,6 +382,10 @@ china_province: [
         },{
             name:"Canada",
             case: 4,
+            death: 0,
+        },{
+            name:"India",
+            case: 3,
             death: 0,
         },{
             name:"United Kingdom",
@@ -395,10 +403,6 @@ china_province: [
             name:"Philippines",
             case: 2,
             death: 1,
-        },{
-            name:"India",
-            case: 2,
-            death: 0,
         },{
             name:"Cambodia",
             case: 1,
@@ -1455,7 +1459,7 @@ module.exports = {
     //apputil.CONSOLE_LOG("App getLatestCoronaDateOn")
     if (true || req.user) {
       let latestData = {
-        updatedOn: "2020-02-03T02:00:00.017Z",
+        updatedOn: "2020-02-03T15:00:00.007Z",
       }
       res.status(200).send(latestData)
     } else {
