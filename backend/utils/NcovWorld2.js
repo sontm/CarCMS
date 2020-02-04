783f10a30177385b7c396bfb54a9fc758283301a
SonTran <sontm.uet.vnu@gmail.com>
2020-02-04 20:10:07 +0700

    02/04 refactor code, divide data files

---
+++ backend/utils/NcovWorld2.js
@@ -0,0 +1,186 @@
+export default {
+    data:[ 
+        { 
+            "date":"2020-02-05T00:30:00.007Z",
+            "world":{ 
+                "case":20697,
+                "death":427,
+                "curve":730,
+                "risk":"Very High"
+            },
+            "tranmission_rate_min":3.0,
+            "tranmission_rate_max":4.0,
+            "fatality_rate":2,
+            "incubation_period_min":2,
+            "incubation_period_max":14,
+            "countries":[ 
+                { 
+                    "name":"Mainland China",
+                    "case":20485,
+                    "death":425,
+                    "suspect":null,
+                    "isolate":null,
+                    "severe":2788,
+                    "curve":719,
+                    "risk":"Very High"
+                },
+                { 
+                    "name":"Thailand",
+                    "case":25,
+                    "death":0,
+                    "curve":7
+                },
+                { 
+                    "name":"Singapore",
+                    "case":24,
+                    "death":0
+                },
+                { 
+                    "name":"Japan",
+                    "case":20,
+                    "death":0
+                },
+                { 
+                    "name":"Hong Kong",
+                    "case":17,
+                    "death":1
+                },
+                { 
+                    "name":"South Korea",
+                    "case":16,
+                    "death":0
+                },
+                { 
+                    "name":"Australia",
+                    "case":13,
+                    "death":0,
+                    "curve":2
+                },
+                { 
+                    "name":"Germany",
+                    "case":12,
+                    "death":0
+                },
+                
+                { 
+                    "name":"United States",
+                    "case":11,
+                    "death":0,
+                     "note":"+ 1 Case in Snohomish, Washington. Male, Age 30s from Jan.21.\n\n"
+                        +"+ 2 Cases in Chicago, Illinois. 1 Female at Age 60s from Jan.24; 1 Male husband from Jan.30\n\n"
+                        +"+ 3 Cases in California. 2 from Jan.26 in Orange C., L.A.; 1 Male adult from Jan.31 in Santa Clara C.\n\n"
+                        +"+ 1 Case in Maricopa County, Arizona. 1 Student from Jan.26.\n\n"
+                        +"+ 1 Case in Boston, Massachusetts. 1 Male at Age 20s From Feb.1.\n\n"
+                        +"+ 1 Case in Santa Clara C., California. 1 Female From Feb.2.\n\n"
+                        +"+ 2 Case in San Benito C., California. 1 Male and 1 Female at age 50s From Feb.2.\n\n"
+                },
+                { 
+                    "name":"Taiwan",
+                    "case":10,
+                    "death":0
+                },
+                { 
+                    "name":"Malaysia",
+                    "case":10,
+                    "death":0
+                },
+                { 
+                    "name":"Macao",
+                    "case":10,
+                    "death":0
+                },
+                { 
+                    "name":"Vietnam",
+                    "case":10,
+                    "death":0,
+                    "suspect":304,
+                    "isolate":90,
+                    "curve":3,
+                    "note":"+ 2 bệnh nhân người Trung Quốc.\n\n"
+                        +"+ 3 người Việt Nam, trong đó 2 trường hợp đang được cách ly điều trị tại BC Nhiệt đới cơ sở 2, 1 trường hợp tại BVDK tỉnh Thanh Hoá. Cả 3 đều trở về từ Vũ Hán.\n\n"
+                        +"+ 1 người Việt Nam là lễ tân tại khách sạn Khánh Hoà, tiếp xúc với 2 người Trung Quốc đang điều trị tại Việt Nam.\n\n"
+                        +"+ 1 bệnh nhân có quốc tịch Mỹ, bay về Việt Nam ngày 14/1, quá cảnh tại sân bay Vũ Hán.\n\n"
+                        +"+ Sáng 3/2, Bộ Y tế xác nhận nữ bệnh nhân 29 tuổi, là công nhân trở về từ Vũ Hán, dương tính với nCoV.\n\n"
+                        +"+ Sáng 4/2 xác nhận nam bệnh nhân 30 tuổi ở Vĩnh Phúc là một trong 8 người trở về từ Vũ Hán trên cùng chuyến bay CZ8315 của hãng hàng không Southern China hôm 17/1.\n\n"
+                        +"+ Chiều 4/2 xác nhận ca thứ 10 là nữ 42 tuổi sống ở Vĩnh Phúc, dịp Tết đến chơi nhà cô gái 23 tuổi (ã được xác định dương tính với nCoV) trong đoàn 8 người của công ty Nihon Plast vừa tập huấn ở Vũ Hán, Trung Quốc về.\n\n"
+                },
+                { 
+                    "name":"France",
+                    "case":6,
+                    "death":0
+                },
+                { 
+                    "name":"UAE",
+                    "case":5,
+                    "death":0
+                },
+                { 
+                    "name":"Canada",
+                    "case":4,
+                    "death":0
+                },
+                { 
+                    "name":"India",
+                    "case":3,
+                    "death":0
+                },
+                { 
+                    "name":"United Kingdom",
+                    "case":2,
+                    "death":0
+                },
+                { 
+                    "name":"Russia",
+                    "case":2,
+                    "death":0
+                },
+                { 
+                    "name":"Italy",
+                    "case":2,
+                    "death":0
+                },
+                { 
+                    "name":"Philippines",
+                    "case":2,
+                    "death":1
+                },
+                { 
+                    "name":"Cambodia",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Finland",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Sri Lanka",
+                    "case":1,
+                    "death":0,
+                    "curve":1
+                },
+                { 
+                    "name":"Nepal",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Spain",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Sweden",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Belgium",
+                    "case":1,
+                    "death":0
+                }
+            ]
+        }
+    ]
+};
