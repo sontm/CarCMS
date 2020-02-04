778aa080670e6460110d89e8e2afaa75cb3775cc
SanSanJP <sontm.uet.vnu@gmail.com>
2020-02-04 10:16:48 +0700

    [ncov] 02/04 fix data

--- backend/controllers/nCoVCtrl.js
+++ backend/controllers/nCoVCtrl.js
@@ -1729,7 +1729,7 @@ module.exports = {
     //apputil.CONSOLE_LOG("App getLatestCoronaDateOn")
     if (true || req.user) {
       let latestData = {
-        updatedOn: "2020-01-04T00:30:00.007Z",
+        updatedOn: "2020-01-04T00:30:00.017Z",
       }
       res.status(200).send(latestData)
     } else {
