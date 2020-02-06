ac8fb2e27d43c8a1047f4e94231b2bc51ab959ef
SanSanJP <sontm.uet.vnu@gmail.com>
2020-02-06 13:19:02 +0700

    [ncov] 02/06 update world data

--- backend/utils/NcovWorld2.js
+++ backend/utils/NcovWorld2.js
@@ -1,10 +1,195 @@
 export default {
     data:[ 
         { 
-            "date":"2020-02-05T00:30:00.007Z",
+            "date":"2020-02-06T05:45:00.007Z",
             "world":{ 
-                "case":20697,
-                "death":427,
+                "case":28281,
+                "death":565,
+                "curve":1270,
+                "risk":"Very High"
+            },
+            "tranmission_rate_min":3.0,
+            "tranmission_rate_max":4.0,
+            "fatality_rate":2,
+            "incubation_period_min":2,
+            "incubation_period_max":14,
+            "countries":[ 
+                { 
+                    "name":"Mainland China",
+                    "case":28023,
+                    "death":563,
+                    "suspect":null,
+                    "isolate":null,
+                    "severe":3859,
+                    "curve":1250,
+                    "risk":"Very High"
+                },
+                { 
+                    "name":"Japan",
+                    "case":45,
+                    "death":0
+                },
+                { 
+                    "name":"Singapore",
+                    "case":28,
+                    "death":0,
+                    "curve":1,
+                },
+                { 
+                    "name":"Thailand",
+                    "case":25,
+                    "death":0,
+                    "curve":9
+                },
+                { 
+                    "name":"South Korea",
+                    "case":23,
+                    "death":0,
+                    "curve":1
+                },
+                { 
+                    "name":"Hong Kong",
+                    "case":21,
+                    "death":1
+                },
+                { 
+                    "name":"Australia",
+                    "case":14,
+                    "death":0,
+                    "curve":3
+                },
+                { 
+                    "name":"Germany",
+                    "case":12,
+                    "death":0
+                },
+                { 
+                    "name":"United States",
+                    "case":12,
+                    "death":0,
+                    "curve":1,
+                     "note":"+ 1 Case in Snohomish, Washington. Male, Age 30s from Jan.21.\n\n"
+                        +"+ 2 Cases in Chicago, Illinois. 1 Female at Age 60s from Jan.24; 1 Male husband from Jan.30\n\n"
+                        +"+ 3 Cases in California. 2 from Jan.26 in Orange C., L.A.; 1 Male adult from Jan.31 in Santa Clara C.\n\n"
+                        +"+ 1 Case in Maricopa County, Arizona. 1 Student from Jan.26.\n\n"
+                        +"+ 1 Case in Boston, Massachusetts. 1 Male at Age 20s From Feb.1.\n\n"
+                        +"+ 1 Case in Santa Clara C., California. 1 Female From Feb.2.\n\n"
+                        +"+ 2 Case in San Benito C., California. 1 Male and 1 Female at age 50s From Feb.2.\n\n"
+                },
+                { 
+                    "name":"Taiwan",
+                    "case":11,
+                    "death":0
+                },
+                { 
+                    "name":"Malaysia",
+                    "case":10,
+                    "death":0
+                },
+                { 
+                    "name":"Macao",
+                    "case":10,
+                    "death":0
+                },
+                { 
+                    "name":"Vietnam",
+                    "case":10,
+                    "death":0,
+                    "suspect":304,
+                    "isolate":90,
+                    "curve":3,
+                    "note":"+ 2 bệnh nhân người Trung Quốc.\n\n"
+                        +"+ 3 người Việt Nam, trong đó 2 trường hợp đang được cách ly điều trị tại BC Nhiệt đới cơ sở 2, 1 trường hợp tại BVDK tỉnh Thanh Hoá. Cả 3 đều trở về từ Vũ Hán.\n\n"
+                        +"+ 1 người Việt Nam là lễ tân tại khách sạn Khánh Hoà, tiếp xúc với 2 người Trung Quốc đang điều trị tại Việt Nam.\n\n"
+                        +"+ 1 bệnh nhân có quốc tịch Mỹ, bay về Việt Nam ngày 14/1, quá cảnh tại sân bay Vũ Hán.\n\n"
+                        +"+ Sáng 3/2, Bộ Y tế xác nhận nữ bệnh nhân 29 tuổi, là công nhân trở về từ Vũ Hán, dương tính với nCoV.\n\n"
+                        +"+ Sáng 4/2 xác nhận nam bệnh nhân 30 tuổi ở Vĩnh Phúc là một trong 8 người trở về từ Vũ Hán trên cùng chuyến bay CZ8315 của hãng hàng không Southern China hôm 17/1.\n\n"
+                        +"+ Chiều 4/2 xác nhận ca thứ 10 là nữ 42 tuổi sống ở Vĩnh Phúc, dịp Tết đến chơi nhà cô gái 23 tuổi (ã được xác định dương tính với nCoV) trong đoàn 8 người của công ty Nihon Plast vừa tập huấn ở Vũ Hán, Trung Quốc về.\n\n"
+                },
+                { 
+                    "name":"France",
+                    "case":6,
+                    "death":0
+                },
+                { 
+                    "name":"UAE",
+                    "case":5,
+                    "death":0
+                },
+                { 
+                    "name":"Canada",
+                    "case":5,
+                    "death":0
+                },
+                { 
+                    "name":"India",
+                    "case":3,
+                    "death":0
+                },
+                { 
+                    "name":"Philippines",
+                    "case":3,
+                    "death":1
+                },
+                { 
+                    "name":"United Kingdom",
+                    "case":2,
+                    "death":0
+                },
+                { 
+                    "name":"Russia",
+                    "case":2,
+                    "death":0
+                },
+                { 
+                    "name":"Italy",
+                    "case":2,
+                    "death":0
+                },
+                { 
+                    "name":"Cambodia",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Finland",
+                    "case":1,
+                    "death":0,
+                    "curve":1
+                },
+                { 
+                    "name":"Sri Lanka",
+                    "case":1,
+                    "death":0,
+                    "curve":1
+                },
+                { 
+                    "name":"Nepal",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Spain",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Sweden",
+                    "case":1,
+                    "death":0
+                },
+                { 
+                    "name":"Belgium",
+                    "case":1,
+                    "death":0
+                }
+            ]
+        },
+        { 
+            "date":"2020-02-05T15:00:00.007Z",
+            "world":{ 
+                "case":24554,
+                "death":492,
                 "curve":730,
                 "risk":"Very High"
             },
@@ -16,13 +201,18 @@ export default {
             "countries":[ 
                 { 
                     "name":"Mainland China",
-                    "case":20485,
-                    "death":425,
+                    "case":24363,
+                    "death":491,
                     "suspect":null,
                     "isolate":null,
-                    "severe":2788,
+                    "severe":3219,
                     "curve":719,
                     "risk":"Very High"
+                },
+                { 
+                    "name":"Japan",
+                    "case":33,
+                    "death":0
                 },
                 { 
                     "name":"Thailand",
@@ -36,18 +226,13 @@ export default {
                     "death":0
                 },
                 { 
-                    "name":"Japan",
-                    "case":20,
-                    "death":0
-                },
-                { 
                     "name":"Hong Kong",
-                    "case":17,
+                    "case":18,
                     "death":1
                 },
                 { 
                     "name":"South Korea",
-                    "case":16,
+                    "case":18,
                     "death":0
                 },
                 { 
@@ -61,7 +246,6 @@ export default {
                     "case":12,
                     "death":0
                 },
-                
                 { 
                     "name":"United States",
                     "case":11,
@@ -76,7 +260,7 @@ export default {
                 },
                 { 
                     "name":"Taiwan",
-                    "case":10,
+                    "case":11,
                     "death":0
                 },
                 { 
@@ -125,6 +309,11 @@ export default {
                     "death":0
                 },
                 { 
+                    "name":"Philippines",
+                    "case":3,
+                    "death":1
+                },
+                { 
                     "name":"United Kingdom",
                     "case":2,
                     "death":0
@@ -138,11 +327,6 @@ export default {
                     "name":"Italy",
                     "case":2,
                     "death":0
-                },
-                { 
-                    "name":"Philippines",
-                    "case":2,
-                    "death":1
                 },
                 { 
                     "name":"Cambodia",
